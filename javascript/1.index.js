c92b9f47067b63aeb938ccc7f84c3d4a4163b0bd
KingRavo18 <rkingovskis14@gmail.com>
2025-08-01 11:02:46 +0300

    javascrip-1

---
+++ javascript/1.index.js
@@ -0,0 +1,17 @@
+console.log(`I am Javascript`);
+console.log(`I like the nuclear launch codes`);
+
+//window.alert(`This is an alert`);
+//window.alert(`I like banana`);
+
+document.getElementById(`myH1`).textContent = `Hello Word`;
+document.getElementById(`myP`).textContent = `add`;
+
+// This is a comment
+
+/* 
+This
+is
+a
+comment
+*/
