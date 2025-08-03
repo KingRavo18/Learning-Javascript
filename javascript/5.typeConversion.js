6aafa947f889cc3320735cbcf45ea8fd0081de36
KingRavo18 <rkingovskis14@gmail.com>
2025-08-03 19:44:25 +0300

    t

---
+++ javascript/5.typeConversion.js
@@ -0,0 +1,25 @@
+// type conversion = change the datatype of a value to another
+//                   (strings, numbers, booleans)
+
+//let age = window.prompt("How old are you?");
+
+//age = Number(age);
+
+//age += 1;
+
+//console.log(age, typeof age);
+
+//NaN = Not a Number
+
+let x;
+let y;
+let z; 
+
+x = Number(x);
+console.log(x, typeof x);
+
+y = String(y);
+console.log(y, typeof y);
+
+z = Boolean(z);
+console.log(z, typeof z);
