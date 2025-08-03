45b725d4a102cb3cea4edd5ff7f502cac744dbdc
KingRavo18 <rkingovskis14@gmail.com>
2025-08-03 17:43:08 +0300

    4th

---
+++ javascript/4.userInput.js
@@ -0,0 +1,19 @@
+// How to accept user input
+
+// 1. EASY WAY = window prompt
+/*
+let username;
+
+username = window.prompt("What's your username?");
+
+console.log(username);
+*/
+
+
+// 2. PROFESSIONAL WAY - HTML textbox
+
+let username;
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello, ${username}`;
+}
