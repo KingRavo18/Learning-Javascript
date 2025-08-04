75fd0169bdd1b6a7df8e601e74a56a370748b7cc
KingRavo18 <rkingovskis14@gmail.com>
2025-08-04 14:04:45 +0300

    amht

---
+++ javascript/8.math.js
@@ -0,0 +1,26 @@
+// Math = built-in object that provides a 
+//        collection of properties and methods
+
+//console.log(Math.PI);
+//console.log(Math.E);
+
+let x = 9;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x); //Eliminiates any decimal portion;
+//z = Math.pow(x, y); //X to the power of Y
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.cos(x);
+//z = Math.sin(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+//z = Math.sign(x); // If positive, then , if negative, then -1, if 0, then 0
+//let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(min);
