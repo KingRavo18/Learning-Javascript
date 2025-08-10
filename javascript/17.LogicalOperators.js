9fb60c6311b8d487cbfb65bb64625c9acbaf9ed5
KingRavo18 <rkingovskis14@gmail.com>
2025-08-10 13:40:11 +0300

    d

---
+++ javascript/17.LogicalOperators.js
@@ -0,0 +1,24 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true or false)
+
+//                     AND = &&
+//                     OR  = ||
+//                     NOT = !
+
+/*
+const temp = 20;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is GOOD");
+}else if(temp <= 0 || temp > 30){
+    console.log("The weather is BAD");
+}
+*/
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log("It is CLOUDY");
+}else{
+    console.log("It is SUNNY"); 
+}
