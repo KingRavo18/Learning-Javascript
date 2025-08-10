dbeb414ba646cd47b7ab5c1a8157f997f91a30e9
KingRavo18 <rkingovskis14@gmail.com>
2025-08-10 19:32:04 +0300

    d

---
+++ javascript/18.StrictEquality.js
@@ -0,0 +1,13 @@
+//   = assignment operator
+//  == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatypes are equal)
+//  != inequality operator
+// !== strict inequality operator
+
+const PI = 3.14;
+
+if(PI !== "3.14"){
+    console.log("FALSE");
+}else{
+    console.log("TRUE");
+}
