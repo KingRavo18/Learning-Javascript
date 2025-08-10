de1f1e47fbdafc1aac1f771a07bf182a1a9899f6
KingRavo18 <rkingovskis14@gmail.com>
2025-08-10 09:07:24 +0300

    g

--- javascript/16.MethodChaining.js
+++ javascript/16.MethodChaining.js
@@ -1,7 +1,21 @@
 // Method Chaining = Calling one method after another
 //                   in one continuous line of code.
 
+let username = window.prompt("Enter your username");
+
 // ----- NO METHOD CHAINING -----
+/*
+username = username.trim();
+let firstLetter = username.charAt(0);
+firstLetter = firstLetter.toUpperCase();
+let otherLetters = username.slice(1);
+otherLetters = otherLetters.toLowerCase();
 
-
+username = firstLetter + otherLetters;
+console.log(username);
+*/
 // ----- METHOD CHAINING -----
+
+username = username.trim().charAt(0).toUpperCase() + 
+username.slice(1).toLowerCase();
+console.log(username);
