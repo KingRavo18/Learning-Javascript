59804025a7536fea880a693c2a100805d9195f93
KingRavo18 <rkingovskis14@gmail.com>
2025-08-13 15:24:48 +0300

    f

--- javascript/24.TempConversionProgram.js
+++ javascript/24.TempConversionProgram.js
@@ -1,1 +1,18 @@
 // TEMPERATURE CONVERSION PROGRAM
+
+function conversion() {
+    const conversionResult = document.getElementById("conversion-result");
+    const celFahr = document.getElementById("celsius-fahrenheit");
+    const fahrCel = document.getElementById("fahrenheit-celsius");
+    const value = Number(document.getElementById("temp-number").value);
+    let temp;
+    if(celFahr.checked){
+        temp = value * 9 / 5 + 32;
+        conversionResult.textContent = temp.toFixed(2) + "°F";
+    }else if(fahrCel.checked){
+        temp = (value - 32) * 5 / 9;
+        conversionResult.textContent = temp.toFixed(2) + "°C";
+    }else{
+        conversionResult.textContent = "Please check one of the options";
+    }
+}
