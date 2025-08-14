4e75febd0252c1392d228b96d992df236f6ef99b
KingRavo18 <rkingovskis14@gmail.com>
2025-08-14 15:17:26 +0300

    f

---
+++ javascript/26.SpreadOperator.js
@@ -0,0 +1,21 @@
+// spread operator = ... allows an iterable such as an 
+//                   array or string to be expanded
+//                   into seperate elements
+//                   (unpacks the elements)
+
+let numbers = [1, 2, 3, 4, 5];
+let max = Math.max(...numbers);
+let min = Math.min(...numbers);
+
+console.log(min);
+
+let username = "Bobby brown";
+let letters = [...username].join("-");
+
+console.log(letters);
+
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrots", "celery", "potatoes"];
+let foods = [...fruits, ...vegetables, "eggs", "pancakes"];
+
+console.log(foods);
