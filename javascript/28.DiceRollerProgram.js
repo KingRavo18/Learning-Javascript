7ede61dbb7ab4032a6584934da6ad757489a04c0
KingRavo18 <rkingovskis14@gmail.com>
2025-08-17 16:40:08 +0300

    F

--- javascript/28.DiceRollerProgram.js
+++ javascript/28.DiceRollerProgram.js
@@ -1,1 +1,17 @@
 // DICE ROLLER PROGRAM
+
+function DiceRoller() {
+    const diceAmount = document.getElementById("DiceAmount").value;
+    const diceDisplay = document.getElementById("diceBox");
+    const valueDisplay = document.getElementById("displayedValues");
+    const dice = [];
+    const values = [];
+
+    for(let i = 1; i <= diceAmount; i++){
+        const rollResult = Math.floor(Math.random() * 6) + 1;
+        values.push(rollResult);
+        dice.push(`<img src="../images/${rollResult}.png" alt="Dice ${rollResult}"/>`);
+    }
+    valueDisplay.textContent = values.join(", ");
+    diceDisplay.innerHTML = dice.join(" ");
+}
