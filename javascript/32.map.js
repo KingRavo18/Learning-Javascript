ccd9e0c1f2eea9ccdaea641162f86ac1369798ed
KingRavo18 <rkingovskis14@gmail.com>
2025-08-19 13:59:26 +0300

    d

--- javascript/32.map.js
+++ javascript/32.map.js
@@ -1,1 +1,40 @@
-// .map()
+// .map() = accepts a callback and applies that function
+//          to each element of an array, then return a new array
+
+const numbers = [1, 2, 3, 4, 5];
+const cubes = numbers.map(cube);
+
+console.log(cubes);
+
+function square(element) {
+    return element ** 2;
+}
+
+function cube(element) {
+    return element ** 3;
+}
+
+const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const upperCaseStudents = students.map(upperCase);
+const lowerCaseStudents = students.map(lowerCase);
+
+console.log(lowerCaseStudents);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
+
+const dates = ["2024-1-10", "2025-2-20", "2026-3-30"];
+const newDates = dates.map(formatDates);
+
+console.log(newDates);
+
+function formatDates(element){
+    const parts = element.split("-");
+    return `${parts[2]}-${parts[1]}-${parts[0]}`;
+
+}
