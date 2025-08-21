5942ca79fdf87be5d33e5c284f2d8e0b715f6a58
KingRavo18 <rkingovskis14@gmail.com>
2025-08-21 15:07:32 +0300

    s

--- javascript/39.constructor.js
+++ javascript/39.constructor.js
@@ -1,1 +1,19 @@
-// constructor
+// constructor = special method for defining the
+//               properties and methods of objects
+
+function Car(make, model, year, color){
+    this.make = make,
+    this.model = model,
+    this.year = year,
+    this.color = color,
+    this.drive = function(){console.log(`You drive the ${this.make} ${this.model}`)}
+}
+
+const car1 = new Car("Ford", "Mustang", 2024, "red");
+const car2 = new Car("Chevrolet", "Camaro", 2025, "black");
+const car3 = new Car("Dodge", "Charger", 2026, "silver");
+
+
+console.log(car1);
+console.log(car2);
+car3.drive();
