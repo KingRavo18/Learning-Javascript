1559c502759d77808c3d27aeafbefac89bdb1f29
KingRavo18 <rkingovskis14@gmail.com>
2025-08-22 18:23:35 +0300

    d

--- javascript/41.static.js
+++ javascript/41.static.js
@@ -1,1 +1,46 @@
-// static = 
+// static = keyword that defines properties or methods that belong
+//          to a class itself rather than the objects created
+//          from that class (class owns anything static, not the objects)
+
+class MathUtil{
+    static PI = 3.14;   
+
+    static getDiameter(radius){
+        return radius * 2;
+    }
+    static getCircumference(radius){
+        return 2 * this.PI * radius;
+    }
+    static getArea(radius){
+        return this.PI * radius * radius;
+    }
+}
+
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(34));
+console.log(MathUtil.getCircumference(34));
+console.log(MathUtil.getArea(34));
+
+class User{
+    static userCount = 0;
+
+    constructor(username){
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount() {
+        console.log(`There are ${User.userCount} users`);
+    }
+
+    sayHello(){
+        console.log(`Hello, my username is ${this.username}`);
+    }
+}
+
+const user1 = new User("Bob");
+const user2 = new User("adasaf");
+const user3 = new User("anna");
+
+user1.sayHello();
+User.getUserCount();
