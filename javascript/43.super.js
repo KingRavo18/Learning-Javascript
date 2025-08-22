ae36d187244875ab4f3025f9712702579a3f1685
KingRavo18 <rkingovskis14@gmail.com>
2025-08-23 01:16:53 +0300

    d

--- javascript/43.super.js
+++ javascript/43.super.js
@@ -1,1 +1,57 @@
-// super =
+// super = keyword is used in classes to call the constructor or
+//         access the properties and methods of a parent (superclass)
+//         this = this object
+//         super = the parent
+
+class Animal{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    move(speed){
+        console.log(`The ${this.name} moves at a speed of ${speed}km/h`)
+    }
+}
+class Rabbit extends Animal{
+    constructor(name, age, runSpeed){
+        super(name, age);
+        this.runSpeed = runSpeed;
+    }
+
+    run(){
+        console.log(`This ${this.name} can run`);
+        super.move(this.runSpeed);
+    }
+}
+class Fish extends Animal{
+    constructor(name, age, swimSpeed){
+        super(name, age);
+        this.swimSpeed = swimSpeed;
+    }
+
+    swim(){
+        console.log(`This ${this.name} can swim`);
+        super.move(this.swimSpeed);
+    }
+}
+class Hawk extends Animal{
+    constructor(name, age, flySpeed){
+        super(name, age);
+        this.flySpeed = flySpeed;
+    }
+
+    fly(){
+        console.log(`This ${this.name} can fly`);
+        super.move(this.flySpeed);
+    }
+}
+
+const rabbit = new Rabbit("Bugs", 1, 25);
+const fish = new Fish("Nemo", 2, 32);
+const hawk = new Hawk("burd", 3, 235);
+
+console.log(rabbit.name);
+console.log(rabbit.age);
+rabbit.run();
+hawk.fly();
