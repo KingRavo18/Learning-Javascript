917938ad57ef5ee792940ee7c1f70dc1844a2872
KingRavo18 <rkingovskis14@gmail.com>
2025-08-24 19:38:59 +0300

    d

--- javascript/51.closures.js
+++ javascript/51.closures.js
@@ -1,1 +1,70 @@
-// closure = 
+// closure = A function defined inside of another function,
+//           the inner function has access to the variables
+//           and scope of the outer fucntion.
+//           Allow for private variables and state maintenance
+//           Used frequently in JS frameworks: React, Vue, Angular
+
+function outer(){
+
+    let message = "Hello";
+
+    function inner(){
+        console.log(message);
+    }
+
+    inner();
+}
+outer();
+
+function createCounter() {
+
+    let count = 0;
+
+    function increment(){
+        count++;
+        console.log(`Count increased to ${count}`);
+    }
+
+    function getCount(){
+        return count;
+    }
+
+    return {increment, getCount};
+}
+
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+
+console.log(`The current count is ${counter.getCount()}`);
+
+//Example 3
+
+function score(){
+    let score = 0;
+
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points} pts`);
+    }
+
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points} pts`);
+    }
+
+    function getScore(){
+        return score;
+    }
+
+    return{increaseScore, decreaseScore, getScore};
+}
+
+const gameScore = score();
+
+gameScore.increaseScore(2);
+gameScore.increaseScore(23);
+gameScore.decreaseScore(2);
+
+console.log(gameScore.getScore());
