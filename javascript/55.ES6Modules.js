e60af4079a70dd93eceb0cbaf80d6cdb9bfacd85
KingRavo18 <rkingovskis14@gmail.com>
2025-08-25 13:40:09 +0300

    d

--- javascript/55.ES6Modules.js
+++ javascript/55.ES6Modules.js
@@ -1,1 +1,15 @@
-// ES6 Module
+// ES6 Module = An external file that contains reusable code
+//              that can be imported into other JavaScript files.
+//              Write reusale code for many different apps.
+//              Can contain variables, classes, functions ... and more
+//              Introduced as part of ECMAScrip 2015 update
+
+import {PI, getCircumfernce, getArea, getVolume} from './55.modul.js';
+
+console.log(PI);
+const circumference = getCircumfernce(4);
+const area = getArea(4);
+const volume = getVolume(4);
+console.log(circumference);
+console.log(area);
+console.log(volume);
---
+++ javascript/55.modul.js
@@ -0,0 +1,13 @@
+export const PI = 3.14;
+
+export function getCircumfernce(radius){
+    return 2 * PI * radius;
+}
+
+export function getArea(radius){
+    return PI * radius * radius;
+}
+
+export function getVolume(radius){
+    return (4/3) * PI * radius ** 3;
+}
