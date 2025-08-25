ff7f04528935231bb17a99114f7ebfe57fa49236
KingRavo18 <rkingovskis14@gmail.com>
2025-08-25 14:49:00 +0300

    s

--- javascript/56.AsynchronousCode.js
+++ javascript/56.AsynchronousCode.js
@@ -1,1 +1,22 @@
-// synchronous = 
+// synchronous = Executes line by line consecutively in a sequential manner
+//               Code that waits for an operation to complete.
+
+// asynchronous = Allows multiple operations to be performed concurrently without waiting
+//                Doesn't block the execution flow and allows the program to continue
+//                (I/O operations, network requests, fetching data)
+//                Handled with: Callbacks, Promises, Async/Await
+
+function func1(callback){
+    setTimeout(() => {
+        console.log("Task23");
+        callback();
+    }, 3000);
+}
+
+function func2(){
+    console.log("Task 1");
+    console.log("Task 2");
+    console.log("Task 3");
+}
+
+func1(func2);
