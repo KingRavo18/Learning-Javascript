b0434cea1c3e9752c916c339843e13474a9d8386
KingRavo18 <rkingovskis14@gmail.com>
2025-08-25 22:07:49 +0300

    d

--- javascript/54.Stopwatch.js
+++ javascript/54.Stopwatch.js
@@ -1,47 +1,64 @@
-// STOPWATCH PROGRAM
+// STOPWATCH
 
 const display = document.getElementById("display");
-let timer = null;
+let time = null;
 let startTime = 0;
-let elapsedTime = 0;
+let timePassed = 0;
 let isRunning = false;
 
 function start(){
     if(!isRunning){
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(update, 10);
+        startTime = Date.now() - timePassed;
+        time = setInterval(update, 10);
         isRunning = true;
     }
 }
 
 function stop(){
     if(isRunning){
-        clearInterval(timer);
-        elapsedTime = Date.now() - startTime;
+        clearInterval(time);
+        timePassed = Date.now() - startTime;
         isRunning = false;
     }
 }
 
 function reset(){
-    clearInterval(timer);
+    clearInterval(time);
     startTime = 0;
-    elapsedTime = 0;
-    isRunning = false;  
+    timePassed = 0;
+    isRunning = false;
     display.textContent = "00:00:00:00";
 }
-    
+
 function update(){
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-    let seconds = Math.floor(elapsedTime / 1000 % 60);
-    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+    timePassed = Date.now() - startTime;
+    let hours = Math.floor(timePassed / (1000 * 60 * 60));
+    let minutes = Math.floor(timePassed / (1000 * 60) % 60);
+    let seconds = Math.floor(timePassed / 1000 % 60);
+    let milliseconds = Math.floor(timePassed % 1000 / 10);
 
-    hours = String(hours).padStart(2, 0);
-    minutes = String(minutes).padStart(2, 0);
-    seconds = String(seconds).padStart(2, 0);
-    milliseconds = String(milliseconds).padStart(2, 0);
-
+    hours = hours.toString().padStart(2, "0");
+    minutes = minutes.toString().padStart(2, "0");
+    seconds = seconds.toString().padStart(2, "0");
+    milliseconds = milliseconds.toString().padStart(2, "0");
+    
     display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
+
+
+/*
+start()
+1000 = 1000 - 0
+0 = 1000 - 1000 
+
+10 passes
+
+stop()
+10 = 1010 - 1000
+
+10 passes
+
+start()
+1010 = 1020 - 10
+10 = 1020 - 1010 
+*/
