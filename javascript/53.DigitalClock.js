c5963d3bfefdd03f49a9d3e680cf72d155c44521
KingRavo18 <rkingovskis14@gmail.com>
2025-08-25 17:18:03 +0300

    g

--- javascript/53.DigitalClock.js
+++ javascript/53.DigitalClock.js
@@ -1,15 +1,13 @@
-// DIGITAL CLOCK
-
-function digitalClock() {
-    const date = new Date()
-    let hours = date.getHours();
+function digitalClock(){
+    const time = new Date();
+    let hours = time.getHours();
     const meridiem = hours >= 12 ? "PM" : "AM";
     hours = hours % 12 || 12;
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
     const padZero = (time) => time < 10 ? "0" + time : time;
-    const time = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
-    document.getElementById("digitalClock").textContent = time;
+
+    document.getElementById("digitalClock").textContent = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
 }
 
 digitalClock();
