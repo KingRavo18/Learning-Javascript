d6894d4314ea411922e6a7689da10f4fe0a436e3
KingRavo18 <rkingovskis14@gmail.com>
2025-08-26 17:43:45 +0300

    d

--- javascript/58.Calculator.js
+++ javascript/58.Calculator.js
@@ -1,1 +1,19 @@
 // CALCULATOR
+
+const display = document.getElementById("display");
+
+const addToDisplay = (input) => {
+    display.textContent += input;
+}
+const clearDisplay = () => display.textContent = "";
+
+function calculations(){
+    try {
+        display.textContent = eval(display.textContent);
+        if(display.textContent = Infinity){
+            throw new Error();
+        }
+    } catch(error){
+        display.textContent = "ERROR";
+    }
+}
