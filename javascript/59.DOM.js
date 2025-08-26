2e3d6a6d322ebd3d59136a6510fbee6dc8d2e7d1
KingRavo18 <rkingovskis14@gmail.com>
2025-08-26 20:40:55 +0300

    f

--- javascript/59.DOM.js
+++ javascript/59.DOM.js
@@ -1,1 +1,17 @@
-// DOM
+// DOM = DOCUMENT OBJECT MODEL
+//       Object() that represents the page you see in the web browser
+//       and provides you with an API to interact with it.
+//       Web browser constructs the DOM when it loads the HTML docoument,
+//       and structures all the elements in a tree-like representation.
+//       Javascript can access the DOM to dynamically
+//       change the content, structure, and style of a web page.
+
+document.title = "banana";
+document.body.style.backgroundColor = "gray";
+
+
+console.dir(document);
+
+const username = "Banana";
+const welcomeMessage = document.getElementById("myH1");
+welcomeMessage.textContent += username === "" ? ` Guest` : ` ${username}`;
