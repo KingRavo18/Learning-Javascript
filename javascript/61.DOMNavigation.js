1ab27439eb06baf31f47c7d935b8e4a60a31fd8c
KingRavo18 <rkingovskis14@gmail.com>
2025-08-27 21:08:47 +0300

    r

--- javascript/61.DOMNavigation.js
+++ javascript/61.DOMNavigation.js
@@ -1,1 +1,57 @@
-// DOM Navigation = 
+// DOM Navigation = The process of navigating through the structure
+//                  of an HTML document using JavaScript
+
+// .firstElementChild
+// .lastElementChild
+// .nextElementSibling
+// .previousElementSibling
+// .parentElement
+// .children
+
+// ------------- .firstElementChild -------------
+/*
+const element = document.getElementById("fruits");
+const firstChild = element.firstElementChild;
+firstChild.style.backgroundColor = "yellow";
+
+const ulElements = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+    const firstChild = ulElement.firstElementChild;
+    firstChild.style.backgroundColor = "lightgreen";
+});
+*/
+
+// ------------- .lastElementChild -------------
+/*
+const element = document.getElementById("fruits");
+const lastChild = element.lastElementChild;
+lastChild.style.backgroundColor = "red";
+
+const ulElements = document.querySelectorAll("ul");
+ulElements.forEach(ulElement => {
+    const lastChild = ulElement.lastElementChild;
+    lastChild.style.backgroundColor = "lightblue";
+});
+*/
+
+// ------------- .nextElementSibling -------------
+
+//document.getElementById("apple").nextElementSibling.style.backgroundColor = "blue";
+
+// ------------- .previousElementSibling -------------
+
+//document.getElementById("banana").previousElementSibling.style.backgroundColor = "pink";
+
+// ------------- .parentElement -------------
+
+//document.getElementById("apple").parentElement.style.backgroundColor = "pink";
+
+// ------------- .children -------------
+
+const element = document.getElementById("fruits").children;
+
+Array.from(element).forEach(fruit => {
+    fruit.style.backgroundColor = "pink";
+});
+
