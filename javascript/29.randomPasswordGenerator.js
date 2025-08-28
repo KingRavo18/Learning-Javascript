3f1f498f031c02fb201bcab19ce7d9fec769db73
KingRavo18 <rkingovskis14@gmail.com>
2025-08-29 02:45:35 +0300

    e

--- javascript/29.randomPasswordGenerator.js
+++ javascript/29.randomPasswordGenerator.js
@@ -1,36 +1,50 @@
 // RANDOM PASSWORD GENERATOR
 
-function generatePassword(lenght, includesUpperCase, includesLowerCase, includesNumbers, includesSymbols){
-    
-    if(!includesUpperCase && !includesLowerCase && !includesNumbers && !includesSymbols || lenght <= 0){
-        return "Password contains nothing.";
+function randomPassword() {
+    const HasLowerCaseChars = document.getElementById("LowerCaseChars"),
+          HasUpperCaseChars = document.getElementById("UpperCaseChars"),
+          HasNumbers = document.getElementById("Numbers"),
+          HasSymbols = document.getElementById("Symbols"),
+          LengthInput = document.getElementById("LengthInput"),
+          PasswordLength = Number(LengthInput.value),
+          Result = document.getElementById("Result");
+          
+    if(!HasLowerCaseChars.checked && !HasUpperCaseChars.checked && !HasNumbers.checked && !HasSymbols.checked){
+        Result.textContent = "Please check a password parameter";
+        return;
     }
 
-    let result = "";
-    let letters = "";
-
-    includesUpperCase ? letters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
-    includesLowerCase ? letters += "abcdefghijklmnopqrstuvwxyz" : "";
-    includesNumbers ? letters += "0123456789" : "";
-    includesSymbols ? letters += "!+=-@#$%^&*()" : "";
-
-    let numberOfChars = letters.length;
-    for(let i = 0; i < lenght; i++){
-        result += letters.charAt(Math.floor(Math.random() * numberOfChars));
+    if(PasswordLength === 0 || PasswordLength < 0){
+        Result.textContent = "Please enter a positive number";
+        LengthInput.value = "";
+        LengthInput.focus();
+        return;
     }
-    return result;
+
+    let chars = "",
+        availableChars,
+        password = [],
+        random;
+
+        if(HasLowerCaseChars.checked){
+            chars += "abcdefghijklmnopqrstuvwxyz";
+        }
+        if(HasUpperCaseChars.checked){
+            chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if(HasNumbers.checked){
+            chars += "0123456789";
+        }
+        if(HasSymbols.checked){
+            chars += "!@#$%^&*()_-+=";
+        }
+
+        availableChars = chars.split("");
+
+    for(let i = 0; i < PasswordLength; i++){
+        random = Math.floor(Math.random() * availableChars.length + 1);
+        password.push(availableChars[random]);
+    }
+
+    Result.textContent = password.join("");
 }
-
-const passwordLenght = 20;
-const passwordUpperCase = false;
-const passwordLowerCase = true;
-const includeNumbers = false;
-const includeSymbols = true;
-
-const password = generatePassword(passwordLenght,
-                                  passwordUpperCase,    
-                                  passwordLowerCase,
-                                  includeNumbers,
-                                  includeSymbols);
-
-console.log(password);
