0f64099e73a2d69b35e69e77ecefccd9b4851877
KingRavo18 <rkingovskis14@gmail.com>
2025-08-29 13:38:43 +0300

    d

--- javascript/29.randomPasswordGenerator.js
+++ javascript/29.randomPasswordGenerator.js
@@ -6,7 +6,7 @@ function randomPassword() {
           HasNumbers = document.getElementById("Numbers"),
           HasSymbols = document.getElementById("Symbols"),
           LengthInput = document.getElementById("LengthInput"),
-          PasswordLength = Number(LengthInput.value),
+          PasswordLength = Number(LengthInput.value) || 0,
           Result = document.getElementById("Result");
           
     if(!HasLowerCaseChars.checked && !HasUpperCaseChars.checked && !HasNumbers.checked && !HasSymbols.checked){
@@ -14,8 +14,19 @@ function randomPassword() {
         return;
     }
 
-    if(PasswordLength === 0 || PasswordLength < 0){
-        Result.textContent = "Please enter a positive number";
+    if(PasswordLength < 0 || PasswordLength > 128 || isNaN(PasswordLength) || PasswordLength === 0){
+        if(PasswordLength < 0){
+            Result.textContent = "Please enter a number above zero";
+        }
+        if(PasswordLength > 128){
+            Result.textContent = "Please choose a shorter lenght for your password";
+        }
+        if(isNaN(PasswordLength)){
+            Result.textContent = "Please enter a number";
+        }
+        if(PasswordLength === 0){
+            Result.textContent = "Please choose the lenght for your password";
+        }
         LengthInput.value = "";
         LengthInput.focus();
         return;
@@ -26,23 +37,15 @@ function randomPassword() {
         password = [],
         random;
 
-        if(HasLowerCaseChars.checked){
-            chars += "abcdefghijklmnopqrstuvwxyz";
-        }
-        if(HasUpperCaseChars.checked){
-            chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        }
-        if(HasNumbers.checked){
-            chars += "0123456789";
-        }
-        if(HasSymbols.checked){
-            chars += "!@#$%^&*()_-+=";
-        }
+        if(HasLowerCaseChars.checked){ chars += "abcdefghijklmnopqrstuvwxyz"; }
+        if(HasUpperCaseChars.checked){ chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
+        if(HasNumbers.checked){ chars += "0123456789"; }
+        if(HasSymbols.checked){ chars += "!@#$%^&*()_-+="; }
 
         availableChars = chars.split("");
 
     for(let i = 0; i < PasswordLength; i++){
-        random = Math.floor(Math.random() * availableChars.length + 1);
+        random = Math.floor(Math.random() * availableChars.length);
         password.push(availableChars[random]);
     }
 
