5fb63e72608edffb8b10d9dcaee38c47e71e36d0
KingRavo18 <rkingovskis14@gmail.com>
2025-08-30 22:23:39 +0300

    d

--- javascript/69.ImageSlider.js
+++ javascript/69.ImageSlider.js
@@ -1,1 +1,41 @@
 // IMAGE SLIDER
+
+const images = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+//initializeSlider();
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(images.length > 0){
+        images[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+function showSlide(index){
+
+    if(index >= images.length){
+        slideIndex = 0;
+    }
+    if(index < 0){
+        slideIndex = images.length - 1;
+    }
+
+    images.forEach(image => {
+        image.classList.remove("displaySlide");
+    });
+    images[slideIndex].classList.add("displaySlide");
+}
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide(){
+    clearInterval(intervalId);
+    initializeSlider();
+    slideIndex++;
+    showSlide(slideIndex);
+}
