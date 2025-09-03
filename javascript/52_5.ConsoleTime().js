d6f9fee8a4b061640592ffb1d3260390fb25f5b1
KingRavo18 <rkingovskis14@gmail.com>
2025-09-03 18:59:31 +0300

    /

--- javascript/52_5.ConsoleTime().js
+++ javascript/52_5.ConsoleTime().js
@@ -1,1 +1,35 @@
-// console.time() = 
+// console.time() = tool that allows you to measure the time it tkes
+//                  for a section of code or process to execute
+//                  Great for identifying performance "bottlenecks"
+
+// console.time("label");
+// console.timeEnd("label");
+
+console.time("test");
+
+for(let i = 0; i < 100000; i++){
+    // do some code
+}
+
+console.timeEnd("test");
+
+function loadData(){
+
+    console.time("loadData"); 
+    for(let i = 0; i < 1000000000; i++){
+        // pretend to load data
+    }
+    console.timeEnd("loadData");
+}
+
+function processData(){
+
+    console.time("processData"); 
+    for(let i = 0; i < 1000000; i++){
+        // pretend to load data
+    }
+    console.timeEnd("processData");
+}
+
+loadData();
+processData();
