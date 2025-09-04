5bb5ae17b784bc67903399291e8e6eaa71df46a7
KingRavo18 <rkingovskis14@gmail.com>
2025-09-04 13:18:00 +0300

    F

--- javascript/52_5.FormatCurrency.js
+++ javascript/52_5.FormatCurrency.js
@@ -1,1 +1,20 @@
-// .toLocaleString() = 
+// .toLocaleString() = returns a string with a language
+//                     sensitive representation of a number
+
+// number.toLocaleString("locale", {options});
+
+// 'locale' = specify the language (undefined = default set in the browser)
+// 'options' = object with formatting options
+
+let number = 123456.789;
+
+//number = number.toLocaleString("lv-LV");
+//number = number.toLocaleString("hi-IN");
+//number = number.toLocaleString("de-DE");
+//number = number.toLocaleString(undefined);
+
+//number = number.toLocaleString("lv-LV", {style: "currency", currency: "EUR"});
+//number = number.toLocaleString("hi-IN", {style: "currency", currency: "INR"});
+number = number.toLocaleString("de-DE", {style: "currency", currency: "EUR"});
+
+console.log(number);
