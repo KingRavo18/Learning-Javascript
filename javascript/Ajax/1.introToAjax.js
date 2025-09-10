0956c170421cef768efe6f3792ff74b3c4568c59
KingRavo18 <rkingovskis14@gmail.com>
2025-09-10 10:12:54 +0300

    d

---
+++ javascript/Ajax/1.introToAjax.js
@@ -0,0 +1,14 @@
+//Ways of Ajax
+
+// - Regular AJAX Requests
+// - Fetch API
+// - Axios Library
+// - Jquery Library
+// - Node JS HTTP Module
+
+//Regular AJAX with JS
+
+// - Basic AJAX Requests Flow
+// - Custom API with AJAX for GET, POST, PUT & DELETE
+// - Example App for CRUD Operations
+// - Employee Portal App Using Custom AJAX API
